516a8de028e87390c39801d0000e661a344c6633
weijiaming2012 <1459598490@qq.com>
2019-08-19 11:16:18 +0800

    connect.py commit

--- connect.py
+++ connect.py
@@ -6,6 +6,11 @@ print "Creating socket..."
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 print "done."
 
+print "Looking up port number..."
+port=socket.getservbyname('http','tcp')
+print port
+print "done."
+
 print "Connecting to remote host..."
 s.connect(("www.baidu.com",80))
 print "done."
